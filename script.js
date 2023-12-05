c3e09957b9053eb93b26b08098ac166c712f9d58
eckirubakaran <eckirubakaran@gmail.com>
2023-12-05 17:19:45 +0530

    add photo-webpage master

---
+++ script.js
@@ -0,0 +1,7 @@
+  const menubtn=document.querySelector("#menubar")
+  const items=document.querySelector(".nav-items")
+
+  menubtn.addEventListener("click",()=>{
+  items.classList.toggle("hide")
+
+  })
